891f4401096df66f557b8a0a78dc5f1099706ab9
miloszsedziak <55091915+miloszsedziak@users.noreply.github.com>
2019-11-25 16:40:14 +0100

    Add files via upload

---
+++ zsł/10_Cookie/cookie1.php
@@ -0,0 +1,11 @@
+<?php
+setcookie("age",20);
+setcookie("age",30);
+
+echo $_COOKIE['age'];
+
+
+setcookie("city","Ługowiny",time()+20);
+
+//setcookie("age","", time() - 1);
+ ?>
---
+++ zsł/10_Cookie/cookie2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form method="post">
+      <input type="text" name="imie" placeholder="Wpisz imie"><br><br>
+      <input type="text" name="nazwisko" placeholder="Wpisz Nazwisko"><br><br>
+      <input type="number" name="wiek" placeholder="Wiek"><br><br>
+      <input type="text" name="narodowosc" placeholder="Wpisz Narodowość"><br><br>
+    </form>
+
+<?php
+/*
+  Uzytkownik dodaje w formularzu: imie, nazwisko, wiek i narodowość.
+  Sprawdź czy przesłał wszystkie dane, jeśli tak to wyświetl mu pod formularzem
+    komunikat przesłany zmienna sesyjną o treści "Prawidlowo przesalny formularz".
+  Imie i nazwisko przypisz do zmiennych sesyjnych, wiek i narodowość przypisz
+    do zmiennych cookie.
+  Wyświetl dane na stronie coockieSucces.php w formacie:
+      Imise i Nazwisko : ....
+      Wiek: ....
+      Narodowość: ....
+  Zabeczpiecz formularz przed podaniem danych, które będą miały więcej niż 10
+    znaków, w przypadku błędów wyświetl za pomocą zmiennej sesyjnej
+    komunikat pod formularzem.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
+
+
+  </body>
+</html>
